e6633ef47fc6ca9a330d73aefb62a2a967a6ece2
孟宪明 <xianming.meng@mfashion.com.cn>
2018-08-31 17:24:26 +0800

    1

---
+++ getJdPrice.py
@@ -0,0 +1,24 @@
+import pymysql
+ 
+# 打开数据库连接
+db = pymysql.connect("localhost","root","","test" )
+ 
+# 使用 cursor() 方法创建一个游标对象 cursor
+cursor = db.cursor()
+ 
+# SQL 插入语句
+sql = "INSERT INTO EMPLOYEE(FIRST_NAME, \
+       LAST_NAME, AGE, SEX, INCOME) \
+       VALUES ('%s', '%s', '%d', '%c', '%d' )" % \
+       ('Mac', 'Mohan', 20, 'M', 2000)
+try:
+   # 执行sql语句
+   cursor.execute(sql)
+   # 执行sql语句
+   db.commit()
+except:
+   # 发生错误时回滚
+   db.rollback()
+ 
+# 关闭数据库连接
+db.close()
