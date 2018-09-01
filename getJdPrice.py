fcfe19659e09ab6d81f8c271970837de44f8dfc1
孟宪明 <xianming.meng@mfashion.com.cn>
2018-09-02 01:14:12 +0800

    完成抓取

--- getJdPrice.py
+++ getJdPrice.py
@@ -1,24 +1,53 @@
+#-*- coding: utf-8 -*-
+import urllib.request
+import json
 import pymysql
- 
-# 打开数据库连接
-db = pymysql.connect("localhost","root","","test" )
- 
-# 使用 cursor() 方法创建一个游标对象 cursor
-cursor = db.cursor()
- 
-# SQL 插入语句
-sql = "INSERT INTO EMPLOYEE(FIRST_NAME, \
-       LAST_NAME, AGE, SEX, INCOME) \
-       VALUES ('%s', '%s', '%d', '%c', '%d' )" % \
-       ('Mac', 'Mohan', 20, 'M', 2000)
+import time
+
+db = pymysql.connect("localhost","root","","jd_crawler" )
+
+cursor = db.cursor(cursor=pymysql.cursors.DictCursor)
+
+sql_select = "select * from t_commodity where status=1"
+
 try:
-   # 执行sql语句
-   cursor.execute(sql)
-   # 执行sql语句
-   db.commit()
+  # 执行sql语句
+  cursor.execute(sql_select)
+  # 执行sql语句
+  postData = cursor.fetchall()
 except:
-   # 发生错误时回滚
-   db.rollback()
- 
+  # 发生错误时回滚
+  db.rollback()
+
+for item in postData:
+
+  # 获取skuId对应价格
+  url='http://p.3.cn/prices/mgets?skuIds=J_'+item['skuId']
+
+  request=urllib.request.Request(url)
+  response=urllib.request.urlopen(request)
+  content=response.read()
+
+  result=json.loads(content)
+
+  price=result[0]
+
+  now = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+
+  sql_insert = "INSERT INTO t_price(commodityId, price, updateTime) VALUES (%s, %s, str_to_date(\'%s\','%%Y-%%m-%%d %%H:%%i:%%s'))" % (item['id'], price['p'], now)
+
+  try:
+    # 执行sql语句
+    cursor.execute(sql_insert)
+    db.commit()
+  except:
+    print('insert error')
+    # 发生错误时回滚
+    db.rollback()
+
+else: 
+  print('Success Save Price')
+
+
 # 关闭数据库连接
 db.close()
